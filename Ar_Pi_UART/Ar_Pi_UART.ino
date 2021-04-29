80d13794e74cf2a0587d36a7aa038d6eabf1c36a
Bogus8537 <57387097+Bogus8537@users.noreply.github.com>
2021-04-29 23:53:11 +0900

    Ar_Pi_UART.ino

--- Ar_Pi_UART/Ar_Pi_UART.ino
+++ Ar_Pi_UART/Ar_Pi_UART.ino
@@ -3,7 +3,7 @@
 #define TXP 2
 #define RXP 3
 
-SoftwareSerial Serial_F(TXP, RXP);
+SoftwareSerial Serial_PI(TXP, RXP);
 
 void setup() {
   Serial.begin(9600);
@@ -13,16 +13,19 @@ void setup() {
 }
 
 void loop() {
-  if(Serial_F.available())
+  
+  // Receive the Pi's Data and give it to PC connected with Arduino UNO!
+  if(Serial_PI.available())
   {
-    Serial.write(Serial_F.read());
+    Serial.write(Serial_PI.read());
   }
 
+  // Receive the Arduino UNO's Data and give it to Pi!
   if(Serial.available())
   {
     char data = Serial.read();
     Serial.println(data);
-    Serial_F.write(data);
+    Serial_PI.write(data);
   }
 
 }
